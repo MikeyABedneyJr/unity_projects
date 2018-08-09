e1995569fe7174c3445b61626fb58703c595e150
MikeyABedneyJr <mike.bedney@gmail.com>
2018-08-08 21:52:58 -0700

    finished but there is a bug...

--- Roll a ball/Assets/Roll A Ball Game/Scripts/PlayerController.cs
+++ Roll a ball/Assets/Roll A Ball Game/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
 	private int count;
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour {
 		rb = GetComponent<Rigidbody> ();
 		count = 0;
 		SetCountText ();
+		winText.text = "";
 	}
 
 	void FixedUpdate ()
@@ -37,5 +39,8 @@ public class PlayerController : MonoBehaviour {
 
 	void SetCountText () {
 		countText.text = "Count: " + count.ToString();
+		if (count >= 12){
+			winText.text = "You won!!!";
+		}
 	}
 }
