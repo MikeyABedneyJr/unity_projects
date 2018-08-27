018b8efd55d2d94a8cd6070b9e0fbcb7a6f8a07e
mikeyabedneyjr <mike.bedney@gmail.com>
2018-08-27 13:14:35 -0700

    working on a basic scene

---
+++ AirborneRanger2d/Assets/AirborneRangerTestScene/Scripts/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyMovement : MonoBehaviour {
+
+     public float speed = 1;
+     // Use this for initialization
+     void Start () {
+     }
+	 
+public float movementTimer = 2f;
+public float maxSpeed = 5f;
+private Vector3 movement;
+private float timeLeft;
+ 
+ void Update()
+ {
+   timeLeft -= Time.deltaTime;
+   if(timeLeft <= 0)
+   {
+     movement = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+     timeLeft = movementTimer;
+   }
+   transform.position += movement * Time.deltaTime;
+ }
+
+}
---
+++ AirborneRanger2d/Assets/AirborneRangerTestScene/Scripts/PlayerControls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Why did I have to make this new script? Last time I create script in assets
+// moved to correct location, then tried to drag/drop but it wouldn't show up
+public class PlayerControls : MonoBehaviour {
+
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float axisX = Input.GetAxis("Horizontal");
+		float axisY = Input.GetAxis("Vertical");
+		
+		// Would it not be better for me to use a Vector2 ?
+		transform.Translate(new Vector3(axisX,axisY) * Time.deltaTime * speed);
+	}
+}
